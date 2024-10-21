48e570f34917d12af39947d8797ff9c5fb402cca
Lukachoni <164752055+Lukachoni@users.noreply.github.com>
2024-10-21 20:01:13 +0200

    commit

---
+++ level - 09/homework/python.py
@@ -0,0 +1,17 @@
+#name = "Luka"
+#lastname = "tuslaia"
+#age = "13"
+#country = "Georgia"
+#Favoritecar = "camry"
+#Favoritecolor = "Blue"
+#FavoriteNumber = "69"
+#print (name+ ' ' +lastname+ ' ' +age+ ' ' +country+ ' ' +Favoritecar+ ' ' +Favoritecar+ ' ' +Favoritecolor+ ' ' +FavoriteNumber)
+
+
+num1 =int (input ("Please Enter Your num: "))
+num2 =int (input ("Please Enter Your num: "))
+num3 =int (input ("Please Enter Your num: "))
+print (num1 + num2 + num3)
+print (num1 - num2 - num3)
+print (num1 * num2 * num3)
+print (num1 / num2 / num3)
