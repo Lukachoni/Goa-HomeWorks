5b8e7f88228e833541b4c10dc44dc8442b3dd32e
Lukachoni <164752055+Lukachoni@users.noreply.github.com>
2024-10-21 19:56:40 +0200

    commit

---
+++ level - 05/homework/python.py
@@ -0,0 +1,14 @@
+name ='luka'
+lastname ='tsulaia'
+age ='13'
+print(name+ ' ' +lastname+ ' ' +age)
+
+
+
+
+nam1 =32
+nam2 =12
+print (nam1+nam2)
+print (nam1-nam2)
+print (nam1*nam2)
+print (nam1/nam2)
