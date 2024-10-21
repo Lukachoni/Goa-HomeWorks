fa0dfa5a82e10be41310529c5aec61032e58100b
Lukachoni <164752055+Lukachoni@users.noreply.github.com>
2024-10-21 19:57:09 +0200

    commit

---
+++ level - 07/homework/python.py
@@ -0,0 +1,22 @@
+themenu = "the menu is-xhinkali and pizza peperoni,"
+drinks = "and the beverages are-lemonade,beer,sprite,juice"
+print (themenu+ ' ' +drinks)
+
+
+
+order1 = "xhinkali with sauce"
+estimatedtime = "20 minutes"
+
+order2 = "pizza pepperoni"
+estimatedtime2 = "15 minutes"
+ 
+nam1 =20
+nam2 =15 
+print ("order is xhinkali with sauce,the second order is pizza peperoni")
+
+print ("estimated time to be ready is")
+print (nam1+nam2) 
+print ('the xhinkali is 20 minutes and the peperoni is 15')
+
+print ("if you liked the food please give us 5 star review!")
+
